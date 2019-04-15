b3eaad30f0881eb3efb11105ce1ddff2bb55a641
Eddy <eddyukpong@gmail.com>
2019-04-15 15:49:38 +0300

    This is a test

--- javaTestProject/src/javaTestProject/helloWorld.java
+++ javaTestProject/src/javaTestProject/helloWorld.java
@@ -4,8 +4,9 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 		
-		String hw = "Hello World, againvffdfdfgdd";
-		
+		//String hw = "Hello World, again";
+		String name = JOptionPane.showInputDialog("Please enter your name");
+		JOptionPane.showMessageDialog(null, "Welcome" + name);
 		System.out.println(hw);
 
 	}
