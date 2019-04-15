0524b3cce77d282ff8ba5bbe3ff537a169c9c486
kadityann <kadityann@192.168.0.102>
2019-04-15 14:57:42 +0300

    hb

--- javaTestProject/src/javaTestProject/helloWorld.java
+++ javaTestProject/src/javaTestProject/helloWorld.java
@@ -4,7 +4,7 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 		
-		String hw = "Hello World, againsasdadsasd";
+		String hw = "Hello World, againsasdadsasssd";
 		
 		System.out.println(hw);
 
