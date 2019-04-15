6b49d59c380b45fffbbdbe66a22516d3934c27b6
Emmanuel <emsy_2004@yahoo.com>
2019-04-15 15:55:03 +0300

    Edited hello world

--- javaTestProject/src/javaTestProject/helloWorld.java
+++ javaTestProject/src/javaTestProject/helloWorld.java
@@ -4,7 +4,7 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 		
-		String hw = "Hello World, againvffdfdfgdd";
+		String hw = "Hello World,dont change this";
 		
 		System.out.println(hw);
 
