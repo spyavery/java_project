e9b93f5dde433f90004314a3cb7fc6f4052175ef
Emmanuel <emsy_2004@yahoo.com>
2019-04-20 01:28:34 +0300

    Hello World Change

--- javaTestProject/src/javaTestProject/helloWorld.java
+++ javaTestProject/src/javaTestProject/helloWorld.java
@@ -7,9 +7,9 @@ public class helloWorld {
 	public static void main(String[] args) {
 		
 
-		String hw = "Hello World,dont change this";
+//		String hw = "Hello World,dont change this";
 
-		//String hw = "Hello World, again";
+		String hw = "Hello World, again";
 		String name = JOptionPane.showInputDialog("Please enter your name");
 		JOptionPane.showMessageDialog(null, "Welcome" + name);
 		System.out.println(hw);
